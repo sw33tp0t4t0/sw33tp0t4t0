b331986914730342aad1ad71b054f5fa1aa2c6c6
manhnv <parabol123654@gmail.com>
2022-07-20 01:40:50 +0700

    remove secret_key, turn off debug in settings

--- sw33tp0t4t0/settings.py
+++ sw33tp0t4t0/settings.py
@@ -21,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-to+@(kzpai#)h^ufb+ng$#)1s0j%d@84a%napx2_k+ihmm^g0o'
-SECRET_KEY = 'WmjBWx32tYi08/lw5tH/7XMHWw7pTn5KsvcAJDuWHTk='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
