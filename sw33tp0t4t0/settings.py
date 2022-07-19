664a67c1d91ab67c5bfc92d541786b3703038d04
manhnv <parabol123654@gmail.com>
2022-07-20 00:42:47 +0700

    Remove a hardcoded Django secret key

--- sw33tp0t4t0/settings.py
+++ sw33tp0t4t0/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-to+@(kzpai#)h^ufb+ng$#)1s0j%d@84a%napx2_k+ihmm^g0o'
+# SECRET_KEY = 'django-insecure-to+@(kzpai#)h^ufb+ng$#)1s0j%d@84a%napx2_k+ihmm^g0o'
+# SECRET_KEY = 'WmjBWx32tYi08/lw5tH/7XMHWw7pTn5KsvcAJDuWHTk='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
