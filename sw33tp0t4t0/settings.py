90d960a68d4533f6b272b31a22e2a49bab8bcfbb
manhnv <parabol123654@gmail.com>
2022-08-31 23:27:15 +0700

    add static

--- sw33tp0t4t0/settings.py
+++ sw33tp0t4t0/settings.py
@@ -122,10 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
